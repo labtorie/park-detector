e3c3ebef935ee915e753eac028027f07991a976b
Max Urban <56982278+labtorie@users.noreply.github.com>
2021-08-12 19:52:01 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 
 const Point = (lat, long) => ({latitude: lat, longitude: long})
 
+const redDot = 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Basic_red_dot.png'
+const blueDot = 'https://upload.wikimedia.org/wikipedia/commons/8/8e/Pan_Blue_Circle.png'
+
 // Парк Горького
 const coordinates = [
     Point(55.7218772, 37.6000200),
@@ -34,11 +37,42 @@ const coordinates = [
     Point(55.7218772, 37.6000200),
 ]
 
-console.log(JSON.stringify(coordinates.map(point=>([point.latitude, point.longitude]))))
+//console.log(JSON.stringify(coordinates.map(point=>([point.latitude, point.longitude]))))
 
 const pointInside = Point(55.72735175566863, 37.60143756866456)
 
+function createDotPool (polygon, callback=()=>{}) {
 
+    const {x_max, x_min, y_max, y_min} = polygon.reduce((limits, point)=>{
+        return {
+            x_max: Math.max(limits.x_max, point.longitude),
+            x_min: Math.min(limits.x_min, point.longitude),
+            y_max: Math.max(limits.y_max, point.latitude),
+            y_min: Math.min(limits.y_min, point.latitude),
+        }
+    }, {
+        x_max: polygon[0].longitude,
+        x_min: polygon[0].longitude, 
+        y_min: polygon[0].latitude, 
+        y_max: polygon[0].latitude
+    })
+
+
+    const d_x = x_max - x_min
+    const d_y = y_max - y_min
+    const resX = 30
+    const resY = Math.round(resX * (d_x / d_y))
+    const points = []
+
+    for (let i = 0; i <= resX; i++) {
+        for (let j=0; j <= resY; j++) {
+            points.push(Point(y_min+j*(d_y / resY), x_min+i*(d_x / resX)))
+        }
+    }
+
+    return points
+
+}
 
 // point = [lat, long], vs = [[lat1, long1], [lat2, long2], ...]
 function inside(point, vs) {
@@ -49,9 +83,16 @@ function inside(point, vs) {
     for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
         var xi = vs[i][0], yi = vs[i][1];
         var xj = vs[j][0], yj = vs[j][1];
+
+        const k = (yj-yi) / (xj - xi)
+        const b = yj - k * xj
+        const x_int = (y - b) / k
+
         
-        var intersect = ((yi > y) != (yj > y))
-            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        var intersect = ((yi > y) != (yj > y)) && (x > x_int)
+
+
+
         if (intersect) inside = !inside;
     }
     
@@ -85,6 +126,8 @@ function initMap() {
 
     const parkGorkogo = coordinates.map(point=>({lat: point.latitude, lng: point.longitude}))
 
+    
+
     const park = new google.maps.Polyline({
         path: parkGorkogo,
         geodesic: true,
@@ -94,6 +137,24 @@ function initMap() {
       });
       park.setMap(map);
     
+      const svgMarker = (isIn) => ({
+        path: google.maps.SymbolPath.CIRCLE,
+        fillColor: isIn ? "#43b02a" : 'red',
+        fillOpacity: 1,
+        strokeWeight: 0,
+        rotation: 0,
+        scale: 3,
+        //anchor: new google.maps.Point(15, 30),
+      });
+      
+
+      createDotPool(coordinates).forEach(point=>{
+        const dot = new google.maps.Marker({
+            position: {lat: point.latitude, lng: point.longitude},
+            icon: svgMarker(inside([point.latitude, point.longitude], coordinates.map(point=>[point.latitude, point.longitude]))),
+          }); 
+          dot.setMap(map)
+      })
   
     let infoWindow = new google.maps.InfoWindow({
         content: "Это парк Горького!",
